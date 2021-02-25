de9460d2b789aabf27e19f5fa13a6ef81938c39e
Urs Stettler <boundlessenjoy@gmail.com>
2021-02-25 16:24:10 +0100

    docs: ✏️ comments and small changes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
     const response = await fetchResult.json();
     //console.log(questions);
 
+    // Selectionieren
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
@@ -10,18 +11,31 @@
     let slides = response.length;
     let currentSlide = 0;
 
+   /*  - Durch die gesamte API durch iterieren
+       - Template für die "Frage" erstellen und dies auslesen
+       - Im DOM ausgeben
+       - Template für Korrekte und Falsche Antworten erstellen
+       - Beide im DOM ausgeben
+       - Prev/Next Buttons Selectionieren
+       */
+
+
+    // for..in Loop durch response.results, mit "item" kann man danach darauf zugreifen
     for(const item of response.results) {
         console.log(item)
 
-        const template = 
+        // Template for Question
+        const template =
         `<div class="slide active-slide">
         <div class="question">
         ${item.question}
         </div>
         </div> `;
 
+        // In DOM einfügen
         quizContainer.innerHTML = template;
 
+        // Div für die Korrekte Antwort
         const correct =
         `<div class="answer">
         ${item.correct_answer}
@@ -29,19 +43,30 @@
 
         const slide = document.querySelector(".slide");
 
+        // Korrekt Antwort in .Slide Ausgeben
         slide.innerHTML += correct
-        
+
         for(const itemIncorrect of item.incorrect_answers){
-            //console.log("inncorrecht: " +itemIncorrect)
-        const incorrect = 
+            //console.log("incorrecht: " +itemIncorrect)
+
+        // Div für falsche Antworten
+        const incorrect =
         `<div class="answer">
         ${itemIncorrect}
         </div> `
-        
+
+        // Falsche Antworten in Slide ausgeben
         slide.innerHTML += incorrect
 
         }
-        
+
+        // Prev Button
+        const prevButton = document.querySelector("#previous");
+        const NextButton = document.querySelector("#next");
+
+
+
+
         // Next Prev Button bei click slide ändern. (remove class)
 
 
